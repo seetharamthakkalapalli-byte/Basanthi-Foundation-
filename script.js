93fb9310250e3a20835050c17644455705e6c569
Ramu <seetharamthakkalapalli@gmail.com>
2026-08-17 13:23:09 +0530

    Initial Basanthi Foundation website

---
+++ script.js
@@ -0,0 +1,19 @@
+const donateButton = document.getElementById("donateButton");
+const contactButton = document.getElementById("contactButton");
+const cickButton = document.getElementById("cickButton");
+
+
+donateButton.addEventListener("click", function() {
+    alert("Thank you for helping our community!");
+
+});
+
+contactButton.addEventListener("click", function() {
+    alert("Thank you for contacting Basanthi Foundation!");
+    
+});
+
+cickButton.addEventListener("click", function(){
+    alert("thanks for cicking");
+})
+ 
